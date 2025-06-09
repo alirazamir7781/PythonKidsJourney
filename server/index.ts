9ed65b86cf6e8bb2b76a5ce4790d88e85cc940d0
alirazamir7781 <39628453+alirazamir7781@users.noreply.github.com>
2025-06-09 04:42:29 +0000

    new

--- server/index.ts
+++ server/index.ts
@@ -38,9 +38,6 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  // Initialize database with seed data
-  await seedDatabase();
-  
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
